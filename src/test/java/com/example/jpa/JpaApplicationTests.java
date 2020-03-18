48506b4c09620873394a1737d46925a519bdc404
xutiancheng <xutiancheng@qgutech.com>
2020-03-18 14:19:48 +0800

    测试类调用Controller

--- src/test/java/com/example/jpa/JpaApplicationTests.java
+++ src/test/java/com/example/jpa/JpaApplicationTests.java
@@ -1,15 +1,21 @@
 package com.example.jpa;
 
+import com.example.jpa.controller.UserController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class JpaApplicationTests {
 
-
+    @Resource
+    private UserController userController;
 
     @Test
     void contextLoads() {
+        String all = userController.findAll();
+        System.out.println(all);
 
     }
 
