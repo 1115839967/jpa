8e890c9c399c65c04224513331594e6d896200fe
xutiancheng <xutiancheng@qgutech.com>
2020-03-19 13:49:48 +0800

    user实体主键类型修改，添加注释

--- src/main/java/com/example/jpa/entity/UserEntity.java
+++ src/main/java/com/example/jpa/entity/UserEntity.java
@@ -21,12 +21,21 @@ public class UserEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private String id;
 
+    /**
+     * 用户名
+     */
     private String userName;
 
+    /**
+     * 密码
+     */
     private String password;
 
+    /**
+     * 真实姓名
+     */
     private String realName;
 
 }
